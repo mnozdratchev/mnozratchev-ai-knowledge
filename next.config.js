020e408a546f1bc9c47d8a092cc214b0ed1e973e
MNozdratchev <maik_nozdratch2@hotmail.de>
2024-01-24 20:28:45 +0100

    add standalone

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    distDir : "build",
+    output: "standalone"
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
